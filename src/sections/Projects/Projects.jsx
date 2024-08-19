38d69fe0d00f420c789cfbf197efa998d0b8b6a4
Tifro24 <ElijahDuma@hotmail.com>
2024-08-19 13:20:41 +0100

    changed project link and images

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styles from './ProjectsStyles.module.css'
-import viberr from '../../assets/viberr.png'
-import freshBurger from '../../assets/fresh-burger.png'
-import fitLift from '../../assets/fitlift.png'
-import hipsster from '../../assets/hipsster.png'
+import mario from '../../assets/mario-icon.png'
+import swords from '../../assets/swords-icon.png'
+import sunLogo from '../../assets/sun-icon.png'
+import mysqlLogo from '../../assets/mysql-logo.png'
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -12,26 +12,26 @@ function Projects() {
         <h1 className='sectionTitle'>Projects</h1>
         <div className={styles.projectsContainer}>
            <ProjectCard 
-           src={viberr} 
-           link={'#'} 
+           src={mario} 
+           link={'https://tifro24.github.io/Project-Week-Game/'} 
            h3='Mario Game'
            p = 'Platformer Game (JS)'
            />
             <ProjectCard 
-           src={freshBurger} 
-           link={'#'} 
+           src={swords} 
+           link={'https://tifro24.github.io/Noob-Fight-Game/'} 
            h3='Noob Fight'
            p = 'Fighting Game (JS)'
            />
              <ProjectCard 
-           src={fitLift} 
-           link={'#'} 
+           src={sunLogo} 
+           link={'https://github.com/Tifro24/Python-Project-Weather-App-v2/blob/main/weather_app_v2.py'} 
            h3='Weather App'
            p = '(Python)'
            />
            <ProjectCard 
-           src={hipsster} 
-           link={'#'} 
+           src={mysqlLogo} 
+           link={'https://tifro24.github.io/Anime-Fight-Database/'} 
            h3='Anime Fight Db'
            p = '(MySQL)'
            />
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,8 +1,15 @@
 import styles from './SkillsStyles.module.css'
-import checkMarkIcon from '../../assets/checkmark-dark.svg'
+import checkMarkLight from '../../assets/checkmark-light.svg'
+import checkMarkDark from '../../assets/checkmark-dark.svg'
 import SkillList from '../../common/SkillList';
+import { useTheme} from '../../common/ThemeContext.jsx';
+
+
 
 function Skills() {
+
+  const {theme} = useTheme();
+  const checkMarkIcon = theme === 'light' ? checkMarkLight : checkMarkDark;
   return  (
     <section id='skills' className={styles.container}>
         <h1 className='sectionTitle'>Skills</h1>
